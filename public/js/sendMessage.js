6e8244dd5209a2b62c25ebc2f9c06546be4e2355
Yakovlev Denis <barkov777@gmail.com>
2017-01-30 15:40:46 +0600

    Changed textarea function

--- public/js/sendMessage.js
+++ public/js/sendMessage.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	var socket = io();
 	socket.on('newMess', function(data){
 		var newMessage = "";
+		console.log(data);
 		if (data.from == getCookie('login'))
 		{
 			newMessage += "<div class='message-outher'>" +
@@ -41,13 +42,34 @@ $(document).ready(function(){
 	});
 	socket.emit('setRooms', {login: getCookie('login')});
 	$('.dialog-send-button').on('click', function(){
+		sendMessage();
+	});
+	$(".dialog-message-input").keypress(function(e){
+	    if(e.keyCode == 13 && e.shiftKey){
+	    	var rows = Number($(".dialog-message-input").attr("rows"));
+	    	if (rows < 3) {
+	    		$(".dialog-message-input").attr("rows", rows+=1);
+	    		//$(".dialog-message-input").css("marginTop", "-=9");
+	    		//$(".dialog-message-input").css("marginBottom", "-=9");
+	    		$(".dialog-footer").css("height", "+=10")
+	    		rows = rows + 1;
+	    	}
+	    }
+	    else if (e.keyCode == 13) {
+	    	sendMessage();
+	    }
+	});
+	function sendMessage () {
 		var messBody = $('.dialog-message-input').val();
 		$('.dialog-message-input').val("");
+		$(".dialog-message-input").attr("rows", 1);
+		$(".dialog-footer").css("height", "60px")
+		//$(".dialog-message-input").css("margin", "18px 0");
 		if (messBody) {
 			$.ajax({
 				url: 'api/sendMessage',
 				method: 'put',
-				data: {message: messBody, dialogId: $(this).attr('id')},
+				data: {message: messBody, dialogId: $('.dialog-send-button').attr('id')},
 				success: function(response)
 				{
 					/*console.log(response);
@@ -57,5 +79,5 @@ $(document).ready(function(){
 				}
 			});
 		}
-	});
+	};
 });
